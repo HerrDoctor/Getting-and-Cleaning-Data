940475dc95a2de267ea0559ec54c26225cbce345
HerrDoctor <samuel.vidal.gallego@gmail.com>
2020-09-24 06:42:29 +0200

    Bug fix: Giving variables more verbose names

--- Scripts/run_analysis.R
+++ Scripts/run_analysis.R
@@ -3,7 +3,7 @@
 #Load the needed libraries
 library(dplyr)
 
-#FIRST STEP: Get the data containing the training set, training labels and subject labels (for the training set) 
+#STEP 1: Get the data containing the training set, training labels and subject labels (for the training set) 
 #and merge into a single data frame
 X_train <- read.table("Data/UCI\ HAR\ Dataset/train/X_train.txt", nrows = 7352)
 y_train <- readLines("Data/UCI\ HAR\ Dataset/train/y_train.txt", n = 7352)
@@ -12,7 +12,7 @@ subject_train <- readLines("Data/UCI\ HAR\ Dataset/train/subject_train.txt", n =
 train <- mutate(X_train, ActivityLabel = y_train)
 train <- mutate(train, SubjectId = subject_train)
 
-#SECOND STEP: Get the data containing the test set, test labels and subject labels (for the test set) and merge
+#STEP 2: Get the data containing the test set, test labels and subject labels (for the test set) and merge
 #into a single data frame
 X_test <- read.table("Data/UCI\ HAR\ Dataset/test/X_test.txt", nrows = 7352)
 y_test <- readLines("Data/UCI\ HAR\ Dataset/test/y_test.txt", n = 7352)
@@ -21,23 +21,36 @@ subject_test <- readLines("Data/UCI\ HAR\ Dataset/test/subject_test.txt", n = 73
 test <- mutate(X_test, ActivityLabel = y_test)
 test <- mutate(test, SubjectId = subject_test)
 
-#THIRD STEP: Join train and test repositories into a single one
+#STEP 3: Join train and test repositories into a single one
 dataset <- rbind(train, test)
 
-#FOURTH STEP: Extract only those dataset variables on a mean and standard deviation, and give them appropriate names
+#STEP 4: Extract only those dataset variables on a mean and standard deviation
 #Get the variables
 variables <- readLines("Data/UCI\ HAR\ Dataset/features.txt", n = 561)
 search_pattern <- "-mean\\(\\)|-std\\(\\)"
-variable_names <- sub("^[0-9]+ ", "", grep(search_pattern, variables, value=TRUE))
-variable_names <- sub("\\(\\)", "", grep(search_pattern, variable_names, value=TRUE))
 dataset <- dataset[grepl(search_pattern, variables)]
-names(dataset)[1:(length(dataset)-2)] <- variable_names
 
-#FIFTH STEP: Change numbers in ActivityLabel column by actual activity names (as a factor)
+#STEP 5: Give variables (columns) appropriate names
+#Delete the numbers and space at the beggining of the column names
+variable_names <- sub("^[0-9]+ ", "", grep(search_pattern, variables, value=TRUE))
+#Delete the parenthesis at the end of the column names
+variable_names <- sub("\\(\\)", "", variable_names)
+#Making column names more verbous: t->Time, f->Frequency, Acc->Accelerometer, Gyro->Gyroscope, Mag->Magnitude,
+#mean->Mean, std-> StandardDeviation
+variable_names <- sub("^t", "Time", variable_names)
+variable_names <- sub("^f", "Frequency", variable_names)
+variable_names <- sub("Acc", "Accelerometer", variable_names)
+variable_names <- sub("Gyro", "Gyroscope", variable_names)
+variable_names <- sub("Mag", "Magnitude", variable_names)
+variable_names <- sub("mean", "Mean", variable_names)
+variable_names <- sub("std", "StandardDeviation", variable_names)
+names(dataset)[1:length(variable_names)] <- variable_names
+
+#STEP 6: Change numbers in ActivityLabel column by actual activity names (as a factor)
 activity_names <- sub("^[0-9]+ ", "", readLines("Data/UCI\ HAR\ Dataset/activity_labels.txt", n = 6))
 dataset$ActivityLabel <- factor(dataset$ActivityLabel, labels = activity_names)
 
-#SIXTH STEP: Create groups in dataset based on SubjectId and ActivityLabel, produce a second tidy data set with
+#STEP 7: Create groups in dataset based on SubjectId and ActivityLabel, produce a second tidy data set with
 #the average for each variable for each activity and each subject and write it to the file Output/tidy_data_set.txt
 average_dataset <- dataset %>% group_by(SubjectId, ActivityLabel) %>% summarize_all(mean, na.rm = TRUE)
 write.table(average_dataset, file = "Output/averages_per_activity_and_subject.txt", row.names = FALSE)
