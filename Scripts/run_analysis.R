02a448ebacdfc1f7fbe5057c3485af0fc1235b01
HerrDoctor <samuel.vidal.gallego@gmail.com>
2020-09-24 20:08:51 +0200

    Bug fix: substitute wrong "BodyBody" by "Body" in column names

--- Scripts/run_analysis.R
+++ Scripts/run_analysis.R
@@ -44,6 +44,7 @@ variable_names <- sub("Gyro", "Gyroscope", variable_names)
 variable_names <- sub("Mag", "Magnitude", variable_names)
 variable_names <- sub("mean", "Mean", variable_names)
 variable_names <- sub("std", "StandardDeviation", variable_names)
+variable_names <- sub("BodyBody", "Body", variable_names)
 variable_names <- sub("$", "_Average", variable_names)
 names(dataset)[1:length(variable_names)] <- variable_names
 
